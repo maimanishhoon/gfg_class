75401582e10f73df131862ac2ebcac09fa97f9fd
maimanishhoon <manishq142@gmail.com>
2025-07-14 03:35:15 +0530

    This is the second commit

---
+++ js/conditional.js
@@ -0,0 +1,8 @@
+// Multiplication Table
+// Write a program to print the multiplication table of a given number using a loop.
+
+// let a = 2;
+for(let i = 2 ; 0<i>10; i++ ){
+    i=i*i;
+    console.log(i);
+}
---
+++ js/index.js
@@ -0,0 +1,26 @@
+// console.log("Hello World!");
+
+// console.log(name);
+
+// const name = " punga";
+// name = "kya baat hai"
+// a = 10;
+// console.log(a);
+
+var a = 10;
+function a(n){
+    var b = 20;
+    console.log(b);
+
+    function b(){
+    var c = 30;
+    console.log(c);
+
+    function c(){
+        var c =60;
+        console.log(c);
+    }
+    c();
+}b();
+}a();
+console.log(a);
